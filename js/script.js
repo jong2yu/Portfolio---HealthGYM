ab042805412582aa20b3de8dab17e71dd500eaa4
jong2yu <97148584+jong2yu@users.noreply.github.com>
2022-08-24 17:20:13 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
   $('.slide').slick({
     slide: 'li',        //슬라이드 되어야 할 태그
     infinite : true,     //무한 반복 옵션     
-    slidesToShow : 1,        // 한 화면에 보여질 컨텐츠 개수
+    slidesToShow : 1,        // 컨텐츠 개수
     slidesToScroll : 1,        //스크롤 한번에 움직일 컨텐츠 개수
     speed : 500,     // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
     arrows : true,         // 옆으로 이동하는 화살표 표시 여부
