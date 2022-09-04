8ff6d34904a14e6fbd8b079d2daec816dbfff6e2
jong2yu <jong2yu@gmail.com>
2022-09-04 13:34:46 +0900

    fix

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
     speed : 500,
     arrows : true,
     dots : true,
-    autoplay : false,
+    autoplay : true,
     autoplaySpeed : 2000,
     pauseOnHover : true,
     vertical : false,
